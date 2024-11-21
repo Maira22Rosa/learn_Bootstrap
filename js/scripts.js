1206721e7a8683514573c89ba91020f9d06ae849
Maira22Rosa <maira.rosa@simples.software>
2024-11-21 08:52:44 -0300

    Criado filtro para mostrar o portfólio

--- js/scripts.js
+++ js/scripts.js
@@ -77,4 +77,43 @@ $(document).ready(function () {
     $("#data-area").parallax({ imageSrc: "img/cidadeparallax.png" });
     $("#apply-area").parallax({ imageSrc: "img/pattern.png" });
   }, 250);
+
+  // filtro do portfolio
+
+  // estamos removendo a classe dos outros botões e aplicando apenas no que foi clicado
+  $(".filter-btn").on("click", function () {
+    let type = $(this).attr("id");
+    let boxes = $(".project-box");
+
+    $(".main-btn").removeClass("active");
+    $(this).addClass("active");
+
+    if (type === "dsg-btn") {
+      eachBoxes("dsg", boxes);
+    } else if (type === "dev-btn") {
+      eachBoxes("dev", boxes);
+    } else if (type === "seo-btn") {
+      eachBoxes("seo", boxes);
+    } else {
+      eachBoxes("all", boxes);
+    }
+  });
+
+  function eachBoxes(type, boxes) {
+    if (type === "all") {
+      // exibe todos
+      $(boxes).fadeIn();
+    } else {
+      $(
+        boxes.each(function () {
+          if (!$(this).hasClass(type)) {
+            // esconde
+            $(this).fadeOut("slow");
+          } else {
+            $(this).fadeIn();
+          }
+        })
+      );
+    }
+  }
 });
